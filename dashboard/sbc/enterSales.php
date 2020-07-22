fe8b886900bc48ef48f2faf2f10cb91c7a9624a1
Mark Bouchett <mark@markbouchett.com>
2020-07-22 17:34:58 -0400

    order Sales By Cat 03

---
+++ dashboard/sbc/enterSales.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Enter Sales By Category</title>
+</head>
+<body>
+	<form method="post" action="processEnterSales.php">
+	<input type="date">
+	</form>
+</body>
+</html>
