c801fd07d2c09ad972072ae60bc67cc611905d1d
Mark Bouchett <mark@markbouchett.com>
2020-07-22 17:00:05 -0400

    order Sales By Cat 02

---
+++ dashboard/sbc/index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Sales By Category</title>
+</head>
+<body>
+<a href="enterSales.php">Enter Sales</a><br>
+</body>
+</html>
