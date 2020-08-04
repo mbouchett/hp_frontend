1720e199efa7ffd036a462124d7e7417f981ab1e
Mark Bouchett <mark@markbouchett.com>
2020-08-04 16:54:40 -0400

    sales data05

--- dashboard/sbc/enterSales.php
+++ dashboard/sbc/enterSales.php
@@ -63,7 +63,7 @@ for($i=0; $i<$itemCount; $i++){
 	<tr><td>Date</td><td>Amount</td><td>Category</td></tr>
 	<?php for($i=0; $i<$itemCount; $i++){ ?>
 	<tr>
-		<td><?= $sbc[$i]['sbc_date'] ?></td><td style="text-align: right;"><?= $sbc[$i]['sbc_amt'] ?></td><td><?= $sbc[$i]['dept_ID'] ?></td>
+		<td><?= $sbc[$i]['sbc_date'] ?></td><td style="text-align: right;"><?= $sbc[$i]['sbc_amt'] ?></td><td style="text-align: center;"><?= $sbc[$i]['dept_ID'] ?></td>
 	</tr>
 	<?php } ?>
 	</table>
