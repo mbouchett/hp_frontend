c9656e34c9722ae68eb64e30e874dce844e797f4
Mark Bouchett <mark@markbouchett.com>
2020-08-30 14:33:45 -0400

    behavior letter 02

--- letter/index.php
+++ letter/index.php
@@ -22,7 +22,7 @@ mysqli_close($db);
 
 To The Senators of Chittenden County,<br><br>
  
-	I am writing to you today to express a growing concern regarding safety and civility on our public streets in Burlington. <br>
+	I am writing to you to express a growing concern regarding safety and civility on our public streets in Burlington. <br>
 	The issues of public intoxication, open drug use, public urination and defecation, public sex acts, aggressive and harassing behavior, <br>
 	commandeering and blocking public thoroughfares, sleeping and camping in both private and public spaces have always been with us. <br>
 	Furthermore, community spaces like The Church Street Marketplace have always been especially attractive to people who demonstrate <br>
@@ -34,8 +34,9 @@ To The Senators of Chittenden County,<br><br>
 	or criminal behavior for those engaged in these activities.<br><br>
 		
 	I want to be clear that I am not concerned with a class of individuals here or any individual for that matter. What I am concerned <br>
-	about is how people behave in shared and public places. I believe that my staff, which is composed largely of young women, have a <br>
-	right to walk to and from their jobs in safety and unharassed. I don’t think that citizens and visitors to our town, who often come <br>
+	about is how people behave in shared and public places.I respect The rights of all kinds of people to to utilize and enjoy these spaces <br>
+	in a respectful manner; however, I also believe that my staff, which is composed largely of young women, have a right to walk to and from <br>
+	their jobs in safety and unharassed. I don’t think that citizens and visitors to our town, who often come <br>
 	with their children, ought be subject to aggressive behavior or witness to people’s private bodily functions. <br><br>
 
 	To this end what I am asking for is a point at which a number of misdemeanor citations might be elevated to a criminal citation <br>
